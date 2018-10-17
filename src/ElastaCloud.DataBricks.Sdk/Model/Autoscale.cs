bc7a3250e2d6b0d072bd6d2ecb2786b36196870e
davidcrossland <david@elastacloud.com>
2018-10-17 15:50:25 +0100

    added autoscale

---
+++ src/ElastaCloud.DataBricks.Sdk/Model/Autoscale.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace ElastaCloud.DataBricks.Sdk.Model
+{
+   /// <summary>
+   /// Describes autoscale parameters
+   /// </summary>
+   public class Autoscale
+   {
+      /// <summary>
+      /// Minimum number of workers
+      /// </summary>
+      [JsonProperty("min_workers")]
+      public int MinWorkers { get; set; }
+
+      /// <summary>
+      /// Maximum number of workers
+      /// </summary>
+      [JsonProperty("max_workers")]
+      public int MaxWorkers { get; set; }
+   }
+}
